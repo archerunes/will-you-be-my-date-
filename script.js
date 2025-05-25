092958c58be715d17c982a911048854a7bbc936f
archerunes <gadisagacious@gmail.com>
2025-05-26 00:21:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -57,13 +57,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Sayang, please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, come on say YES...! ❤️"
 ];
 
 let messageIndex = 0;
